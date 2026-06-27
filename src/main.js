776de6f3ac9cdee16468fc203cf5ff8c42c9a9e0
joeylapetina78 <joeylapetina78@gmail.com>
2026-06-27 01:07:57 -0500

    Hide inactive hub panel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { getProcessorOutputs, normalizeProcessorOutput } from "./components/componentRules.js";
-import { Game } from "./game.js?v=docking-services";
+import { Game } from "./game.js?v=hidden-hub";
 import { Processor } from "./systems/processor.js?v=hunter-tuning";
 import { createGameState } from "./state/gameState.js?v=impact-effects";
 
